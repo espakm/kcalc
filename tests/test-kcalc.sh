ca7cbd289edd10c84291899726cf72d30741ede2
Miklos Espak <miklos.espak@motilent.co.uk>
2021-05-02 00:59:52 +0200

    Add system test
    System test added in the form of a bash script. It tests if the app reads from the
    standard input and prints to the standard output. It also tests the command line
    arguments.
    
    This would be much trickier to test from C++, as there is no standard way of capturing
    the stdin and stdout.
    
    This is also not standard, as it requires bash that is not available on every OS.
    (E.g. even Alpine Linux does not have it by default.)

---
+++ tests/test-kcalc.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+fail() {
+    echo "Test failed."
+    exit 1
+}
+
+trap fail ERR
+
+source_dir=$(git rev-parse --show-toplevel)
+build_dir="${source_dir}/build"
+kcalc_binary="${build_dir}/apps/kcalc"
+
+prefix_input=$(cat << EOF
+3
++ 1 2
++ 1 * 2 3
++ * 1 2 3
+- / 10 + 1 1 * 1 2
+- 0 3
+/ 3 2
+EOF
+)
+
+expected_prefix_output=$(cat << EOF
+3
+3
+7
+5
+3
+-3
+1.5
+EOF
+)
+
+prefix_output=$(${kcalc_binary} --prefix <<< "${prefix_input}")
+
+[[ "${prefix_output}" == "${expected_prefix_output}" ]] || fail
+
+infix_input=$(cat << EOF
+( 1 + 2 )
+( 1 + ( 2 * 3 ) )
+( ( 1 * 2 ) + 3 )
+( ( ( 1 + 1 ) / 10 ) - ( 1 * 2 ) )
+EOF
+)
+
+expected_infix_output=$(cat << EOF
+3
+7
+5
+-1.8
+EOF
+)
+
+infix_output=$(${kcalc_binary} --infix <<< "${infix_input}")
+
+[[ "${infix_output}" == "${expected_infix_output}" ]] || fail
+
+infix_output=$(${kcalc_binary} <<< "${infix_input}")
+
+[[ "${infix_output}" == "${expected_infix_output}" ]] || fail
+
+echo "Test passed."
